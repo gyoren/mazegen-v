edb9d7d4ac2b56f0e464f8e1cb11db09299f3a1b
lrtz <gustas848@gmail.com>
2022-01-24 20:06:49 +0200

    Printing of the maze moved to separate function

--- main.v
+++ main.v
@@ -1,4 +1,5 @@
 import os
+import rand
 
 struct Cell {
 	x int
@@ -6,8 +7,15 @@ struct Cell {
 	walls []int
 }
 
+fn print_maze_matrix(mat [][]int) {
+	println(
+		mat.map(it.map(it.str()).join("")).join("\n").replace("0", "  ").replace("1", "██")
+	)
+}
+
 if os.args.len != 2 {
-	println("Usage: ${os.args[0]} [maze width]")
+	println("Usage: ${os.file_name(os.args[0])} [maze width]")
+	exit(1)
 }
 
 // maze creation
@@ -17,18 +25,14 @@ mut maze_matrix := []int{len: 2 * maze_width + 1}.map([]int{len: 2 * maze_width 
 // maze matrix setup and output
 for i in 0..maze_matrix.len {
 	for j in 0..maze_matrix.len {
-		if i in [0, maze_matrix.len - 1] || j in [0, maze_matrix.len - 1] {
+		if i in [0, maze_matrix.len - 1] || j in [0, maze_matrix.len - 1] || (i % 2 == 0 && j % 2 == 0) {
 			maze_matrix[i][j] = 1
-			if j == maze_matrix.len - 1 {
-				println("██")
-			} else {
-				print("██")
-			}
-		} else if i % 2 == 0 && j % 2 == 0 {
-			maze_matrix[i][j] = 1
-			print("██")
-		} else {
-			print("  ")
 		}
 	}
 }
+
+starting_point := [rand.int_in_range(0, maze_width), rand.int_in_range(0, maze_width)].map(it * 2 + 1)
+
+println("Starting point: ${starting_point}")
+
+print_maze_matrix(maze_matrix)
